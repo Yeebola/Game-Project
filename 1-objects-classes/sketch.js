24266183b543682ecf06a72b521c7593f5e6dd4c
Yeebola <35385179+Yeebola@users.noreply.github.com>
2019-10-23 11:50:13 -0700

    qwe
    Added new class and new ball (class is stil broken)

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -1,20 +1,35 @@
 //create a variable to hold one ball
 let b;
 let anotherBall;
-
+let angle;
+let e;
+let changeX;
+let changeY;
+let weapont;
 function setup() {
   createCanvas(800, 400);
   b = new Ball(0, 100,"red"); //make a new ball from the Ball class and call it b.
   anotherBall = new Ball(200,20,"green");
+  e = new Ball(10, 300, "blue");
+ weapont = new Weapon(200,400,"smg")
+ 	background(220);
 }
 
 
 function draw(){
-	background(220);
+
     b.drawBall(); //draw the ball called b (go look in the Ball class for the drawBall function)
     b.moveBall(); //move the ball called b (go look in the Ball class for the moveBall function)
     anotherBall.drawBall();
     anotherBall.moveBall();
+    e.drawBall();
+    e.moveBall();
+    if (keyIsPressed == true){
+      // if(keyCode === "q"){
+        weapont.equipWeapon();
+      }
+
+    // }
 
 }
 
@@ -33,7 +48,42 @@ class Ball {
 		    ellipse(this.x,this.y,10,10);
 	}
 	moveBall(){ //update the location of the ball, so it moves across the screen
-		this.x = this.x+2;
+		this.x = this.x+4;
 		this.y = this.y+.5;
 	}
+}
+
+class Weapon{
+
+constructor(x,y,type){
+  this.x = x;
+  this.y = y;
+  this.type = type;
+}
+equipWeapon(){
+
+if (this.type == "pistol"){
+    push();
+    translate(this.x,this.y);
+    rotate(random(0,60))
+    fill(0);
+    rect(0,0,15,40);
+    rect(15,40,40,50);
+    pop();
+}
+
+if (this.type == "smg"){
+    push();
+    translate(this.x,this.y);
+    rotate(random(0,60))
+    fill(0);
+    rect(0,0,15,40);
+    rect(15,40,50,50);
+    rect(35,40,43,20);
+    pop();
+}
+
+
+}
+
 }
